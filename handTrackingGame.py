511252bde3246928bfc6511e409fb5e89392d818
zullum <zullumsan@gmail.com>
2021-04-09 17:33:43 +0200

    Add hand tracking module

---
+++ handTrackingGame.py
@@ -0,0 +1,34 @@
+import cv2
+import mediapipe as mp
+import time
+import handTrackingModule as htm
+
+
+cTime = 0
+pTime = 0
+
+cap = cv2.VideoCapture(0)
+detector = htm.handDetector()
+while True:
+    success, img = cap.read()
+
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img)
+
+    if len(lmList) != 0:
+        print(lmList[4])
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+    cv2.imshow("Image", img)
+    # cv2.waitKey(1)
+    k = cv2.waitKey(1) & 0xFF
+    # print(k)
+    # 27 is the code for ECS key
+    if k == 27:
+        cv2.destroyAllWindows()
+        break
