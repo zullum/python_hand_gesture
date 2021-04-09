ea0bb928386c8ac7bfda2b72ea8ca9ba920e2d3c
zullum <zullumsan@gmail.com>
2021-04-09 18:08:47 +0200

    Add line between thum tip and index tip

--- volumeHandControl.py
+++ volumeHandControl.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import numpy as np
+import handTrackingModule as htm
 
 
 ##################################
@@ -11,9 +12,28 @@ cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
+detector = htm.handDetector(detectionCon=0.7)
+
 while True:
 
     success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    # print(lmList)
+    if len(lmList) != 0:
+        # landmark values cna be found on https://google.github.io/mediapipe/solutions/hands
+        print(lmList[4], lmList[8])
+
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
 
     cTime = time.time()
     fps = 1/(cTime - pTime)
